551d60ee832e0c2195e0ea7dbd2fd49455578683
jameskokoska <jameskokoska@users.noreply.github.com>
2020-09-12 03:27:31 -0400

    merged front and backend together

--- googleycalendar/src/App.js
+++ googleycalendar/src/App.js
@@ -1,13 +1,33 @@
 import React, {ReactNode, SyntheticEvent} from 'react';
 import { AsyncStorage } from 'AsyncStorage';
 import ApiCalendar from 'react-google-calendar-api';
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import settingsIcon from "./assets/cog-solid.svg"
+
+//TODO:
+//Add sorting by category
+//Add course type
+//Format date
+//Different colours based on course code?
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.handleItemClick = this.handleItemClick.bind(this);
+    this.refreshWholeList = this.refreshWholeList.bind(this);
     this.state ={calendarObjects: ""};
+    var currentHours = new Date().getHours();
+    //Dark mode colors
+    if(currentHours > 19 || currentHours < 7){
+      document.documentElement.style.setProperty('--background', "#121212");
+      document.documentElement.style.setProperty('--font-color', "#fafafa");
+      document.documentElement.style.setProperty('--highlight', "#9e9e9e25");
+      document.documentElement.style.setProperty('--accent', "#1565c0c9");
+    }
   }
 
   componentDidMount() {
@@ -25,23 +45,11 @@ export default class App extends React.Component {
         this.setState({ numEvents: 20 });
       }
     })
+    this.timeoutHandle = setTimeout(()=>{
+        this.refreshWholeList()
+    }, 500);
   }
-
-
-  handleChange(event) {
-    if(event.target.name=="calendarID"){
-      AsyncStorage.setItem('calendarIDKey', event.target.value); // Note: persist input
-      if(event.target.value!=""){
-        ApiCalendar.setCalendar(event.target.value)
-      }
-    } else if(event.target.name=="numEvents"){
-      AsyncStorage.setItem('numEventsKey', event.target.value); // Note: persist input
-      if(event.target.value!=""){
-        ApiCalendar.setCalendar(event.target.value)
-      }
-    }
-    
-  }
+  
       
   handleItemClick(event: SyntheticEvent<any>, name: string): void {
     if (name === 'signIn') {
@@ -71,57 +79,27 @@ export default class App extends React.Component {
             var calendarObjects= result.items
             this.setState({
               calendarObjects: calendarObjects,
+          })
+        });
+    }
+  }
+  
+  refreshWholeList() {
+    if (ApiCalendar.sign)
+      ApiCalendar.listUpcomingEvents(this.state.numEvents)
+        .then(({result}: any) => {
+          var calendarObjects= result.items
+          this.setState({
+            calendarObjects: calendarObjects,
         })
       });
-    }
   }
   
   render(): ReactNode {
     return (
       <div className="screen">
-        <input
-          name="calendarID"
-          onChange={this.handleChange}
-          defaultValue={this.state.calendarID}
-        />
-        <input
-          name="numEvents"
-          onChange={this.handleChange}
-          defaultValue={this.state.numEvents}
-        />
-        <div onClick={(e) => this.handleItemClick(e, "signIn")}>
-          <ButtonStyle label="Login"/>
-        </div>
-        <div onClick={(e) => this.handleItemClick(e, "signOut")}>
-          <ButtonStyle label="Logout"/>
-        </div>
-        <button
-            onClick={(e) => this.handleItemClick(e, 'sign-in')}
-        >
-          sign-in
-        </button>
-        <button
-            onClick={(e) => this.handleItemClick(e, 'sign-out')}
-        >
-          sign-out
-        </button>
-        <button
-            onClick={(e) => this.handleItemClick(e, 'log')}
-        >
-          log
-        </button>
-        <button
-            onClick={(e) => this.handleItemClick(e, 'changeName')}
-        >
-          changename
-        </button>
-        <button
-            onClick={(e) => this.handleItemClick(e, 'populate')}
-        >
-          populate
-        </button>
-        <div></div>
         <TaskList calendarObjects={this.state.calendarObjects}/>
+        <Settings refreshWholeList={this.refreshWholeList}/>
       </div>
     );
   }
@@ -133,6 +111,108 @@ function ButtonStyle(props){
       {props.label}
     </div>
   )
+}
+
+class Settings extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state ={settingsOpen: false};
+    this.loadInputs = this.loadInputs.bind(this);
+  }
+  handleItemClick(event: SyntheticEvent<any>, name: string): void {
+    if (name === 'openSettings') {
+      this.loadInputs();
+      this.setState({
+        settingsOpen: true,
+      })
+    } else if (name==='closeSettings') {
+      this.loadInputs();
+      //Reload events on exit of settings
+      this.props.refreshWholeList()
+      this.setState({
+        settingsOpen: false,
+      })
+    } else if (name === 'signIn') {
+      ApiCalendar.handleAuthClick();
+    } else if (name === 'signOut') {
+      ApiCalendar.handleSignoutClick();
+    }
+  }
+
+  loadInputs() {
+    AsyncStorage.getItem('calendarIDKey').then((value) => {
+      if (value !== null){
+        //read saved text
+        this.setState({ calendarID: value });
+        if(value!== ""){
+          ApiCalendar.setCalendar(value)
+        }
+      }
+    })
+    AsyncStorage.getItem('numEventsKey').then((value) => {
+      if (value !== null){
+        this.setState({ numEvents: value });
+      } else {
+        this.setState({ numEvents: 20 });
+      }
+    })
+  }
+
+  handleChange(event) {
+    if(event.target.name==="calendarID"){
+      AsyncStorage.setItem('calendarIDKey', event.target.value);
+      if(event.target.value!==""){
+        ApiCalendar.setCalendar(event.target.value)
+      }
+    } else if(event.target.name==="numEvents"){
+      AsyncStorage.setItem('numEventsKey', event.target.value);
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        <img alt="open settings" onClick={(e) => this.handleItemClick(e, "openSettings")} src={settingsIcon} className="settingsIcon"/>
+        <Modal show={this.state.settingsOpen} onHide={(e) => this.handleItemClick(e, "closeSettings")} size="lg">
+          <Modal.Header closeButton>
+            <Modal.Title>Settings</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group>
+                <Form.Label>Calendar ID</Form.Label>
+                <Form.Control name="calendarID" onChange={this.handleChange} placeholder="example@group.calendar.google.com" defaultValue={this.state.calendarID}/>
+                <Form.Text className="text-muted">
+                  By keeping this blank it will be the default calendar. To reset this field, remove everything and refresh the webpage.
+                </Form.Text>
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Number of events to load</Form.Label>
+                <Form.Control name="numEvents" onChange={this.handleChange} placeholder="20" defaultValue={this.state.numEvents}/>
+                <Form.Text className="text-muted">
+                  The number of upcoming events to load from the calendar. Reload required.
+                </Form.Text>
+              </Form.Group>
+            </Form>
+            <div onClick={(e) => this.handleItemClick(e, "signIn")} style={{"float":"left"}}>
+              <ButtonStyle label="Login"/>
+            </div>
+            <div onClick={(e) => this.handleItemClick(e, "signOut")} style={{"float":"right"}}>
+              <ButtonStyle label="Logout"/>
+            </div>
+            <div style={{"textAlign":"center", "margin-top":"50px"}}>Exiting settings will load the calendar.</div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={(e) => this.handleItemClick(e, "closeSettings")}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    )
+  }
+  
+  
 }
 
 class TaskList extends React.Component {
@@ -232,11 +312,11 @@ class TaskEntry extends React.Component{
     if(this.state.checked===true){
       textStyle = "line-through";
       checkMark="&#10004;";
-      checkColor="#424242";
+      checkColor="#777777";
       clickActionCheck="uncheckOff";
     }
     return(
-      <tr className="taskEntry">
+      <tr className="taskEntry fadeIn">
         <td className="check" onClick={(e) => this.handleItemClick(e, clickActionCheck)}><div dangerouslySetInnerHTML={{ __html: checkMark}}></div></td>
         <td style={{"textDecoration":textStyle, "color":checkColor}}>{this.props.name}</td>
         <td>{this.props.date}</td>
