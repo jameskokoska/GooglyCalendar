5e529d52b3e3a64da49bf718b70c0e4669618dbb
jameskokoska <jameskokoska@users.noreply.github.com>
2020-10-07 23:46:06 -0400

    Updates

---
+++ precache-manifest.78be8c9799b754f27ce611cabc5a534c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4e963cea77356837fb58e2cda30961b",
+    "url": "/GoogleyCalendar/index.html"
+  },
+  {
+    "revision": "b007541fcadb1b37763f",
+    "url": "/GoogleyCalendar/static/css/2.da69928d.chunk.css"
+  },
+  {
+    "revision": "b581286fc85626b5f1ad",
+    "url": "/GoogleyCalendar/static/css/main.f080852c.chunk.css"
+  },
+  {
+    "revision": "b007541fcadb1b37763f",
+    "url": "/GoogleyCalendar/static/js/2.86e22780.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/GoogleyCalendar/static/js/2.86e22780.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b581286fc85626b5f1ad",
+    "url": "/GoogleyCalendar/static/js/main.731cf9c4.chunk.js"
+  },
+  {
+    "revision": "29827b617be52e2f565f",
+    "url": "/GoogleyCalendar/static/js/runtime-main.b0f46218.js"
+  },
+  {
+    "revision": "5d7c31b284ddb01fc1cbae0edc9ec210",
+    "url": "/GoogleyCalendar/static/media/Calibri-Regular.5d7c31b2.ttf"
+  },
+  {
+    "revision": "6a5bb7443c342ca5b778d1fe42b563f3",
+    "url": "/GoogleyCalendar/static/media/PierSans-Bold.6a5bb744.otf"
+  },
+  {
+    "revision": "ea81863d8319f50cb42d6e14d2b88c87",
+    "url": "/GoogleyCalendar/static/media/PierSans-Light.ea81863d.otf"
+  },
+  {
+    "revision": "239bd63705325a9358e6d0929b8760ae",
+    "url": "/GoogleyCalendar/static/media/PierSans-Regular.239bd637.otf"
+  },
+  {
+    "revision": "e597acabaa68ed548f509cddfb1ab741",
+    "url": "/GoogleyCalendar/static/media/check-solid.e597acab.svg"
+  },
+  {
+    "revision": "9f07a3df80b5eea9cfdcf9616e3d03d5",
+    "url": "/GoogleyCalendar/static/media/cog-solid.9f07a3df.svg"
+  },
+  {
+    "revision": "d9a24d05eed3db4d122adcaf0ac3d956",
+    "url": "/GoogleyCalendar/static/media/info-circle-solid.d9a24d05.svg"
+  },
+  {
+    "revision": "b1b03f76273177176434141d199180c5",
+    "url": "/GoogleyCalendar/static/media/sync-alt-solid.b1b03f76.svg"
+  },
+  {
+    "revision": "4ae0b977ae038dfe185958e3d45113fc",
+    "url": "/GoogleyCalendar/static/media/thumbtack-solid.4ae0b977.svg"
+  }
+]);
